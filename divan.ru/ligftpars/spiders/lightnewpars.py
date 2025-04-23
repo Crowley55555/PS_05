f3a811d35234da0873f0e875e98b7234a641c852
Crowley55555 <lavrovartem0511@gmail.com>
2025-04-23 23:51:53 +0300

    дз выполнено

---
+++ divan.ru/ligftpars/spiders/lightnewpars.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class LightnewparsSpider(scrapy.Spider):
+    name = "lightnewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        lights = response.css("div.WdR1o")
+        for light in lights:
+            yield {
+                "name": light.css("div.lsooF span::text").get(),
+                "price": light.css("div.pY3d2 span::text").get(),
+                "link": light.css("a::attr(href)").get(),
+            }
